311c98f0eb035d7a7ea19a6354f6f9875f76e034
Rostislav <therostikru@gmail.com>
2019-03-01 00:19:14 +0200

    Initial commit

---
+++ ShutOffer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ShutOffer
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int window, int message, int param1, int param2);
+        static void Main(string[] args)
+        {
+            SendMessage(0xFFFF, 0x0112, 0xF170, 0x0002);
+        }
+    }
+}
